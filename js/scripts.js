87bd19675fad59c9dd210bd8022eb6b123198ed6
hyunchanwi <hyunchan09@gmail.com>
2022-10-14 21:50:05 +0900

    2022/10/14. 9.50

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,16 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+window.onload = function () {
+    // const i2 = document.getElementById('i2');
+    // i2.addEventListener('click', function () {
+    //     alert('버튼 이쁨 ㅎ');
+    // })
+
+    const hw = document.getElementById('hw');
+    hw.addEventListener('click', function () {
+        alert('버튼 되게 신기하지? 이거하는데 1시간 동안 했어..');
+    })
+}
